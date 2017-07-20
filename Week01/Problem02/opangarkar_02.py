341bf49435d67e70da24968175f1f301611ca5ff
Omkar Pangarkar <opangarkar3@gatech.edu>
2017-07-20 18:53:51 -0400

    simplify fib stream generation

--- Week01/Problem02/opangarkar_02.py
+++ Week01/Problem02/opangarkar_02.py
@@ -1,22 +1,18 @@
 from __future__ import print_function
 
-# using generators here as we don't know how long we'll need to run the loop
-# this creates a "stream" of fibonacci numbers that we can test for evenness
+# using generators here to create a "stream" of fibonacci numbers that we can
+# test for evenness
 
 def fib(limit=4000000):
-    F = []
-    i = 0
-    while True:
-        if i > 1:
-            F.append(F[i - 1] + F[i - 2])
-        elif i == 0:
-            F.append(1)
-        elif i == 1:
-            F.append(2)
+    F = [1, 2]
+    i = 2 
+
+    for x in F:
+        yield x
+
+    while F[-1] < limit:
+        F.append(F[i - 1] + F[i - 2])
+        yield F[-1]
         i += 1
-        if F[-1] < limit:
-            yield F[-1]
-        else:
-            return
 
 print(sum(x for x in fib() if x % 2 == 0))
